c4eb6e739cf1b399f117b4271329da9edadde046
Nico Abel Laia <66245419+NicoAL95@users.noreply.github.com>
2023-12-10 00:07:32 +0700

    Rename Sitename

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home() {
             type: 'image/jpeg',
           }
         ],
-        siteName: 'SiteName',
+        siteName: 'GDSC HackFest',
       }}
       twitter={{
         handle: '@handle',
