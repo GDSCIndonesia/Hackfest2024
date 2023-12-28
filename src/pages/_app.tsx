e7ed594c35afd270dbc69be91207a0b9f4768f21
Nico Abel Laia <66245419+NicoAL95@users.noreply.github.com>
2023-12-28 23:54:49 +0700

    Add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
+import { useEffect } from 'react';
+import { initGA, logPageView } from '../components/analytics/analytics';
 
 export default function App({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		initGA(); 
+		logPageView(); 
+	  }, []);
 	return (
 		<>
 			<Component {...pageProps} />
