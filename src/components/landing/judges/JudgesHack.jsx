4941d1cfc3a178dd60e4dfaf1514583dda7b21b1
Nico Abel Laia <66245419+NicoAL95@users.noreply.github.com>
2024-01-27 00:30:48 +0700

    Add top 10 judges

--- src/components/landing/judges/JudgesHack.jsx
+++ src/components/landing/judges/JudgesHack.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import style from "./index.module.css"
 import judges from "../../../lib/json/judges.json"
+import toptens from "../../../lib/json/toptens.json"
 
 export default function JudgesHack() {
   return (
@@ -31,6 +32,35 @@ export default function JudgesHack() {
                     </div>
                     <h1>{judge.name}</h1>
                     <p>{judge.role}</p>
+                </div>
+            ))}
+        </motion.div>
+        <motion.div
+				initial={{ opacity: 0, x: 150 }}
+				whileInView={{ opacity: 1, x: 0 }}
+				transition={{
+					duration: 0.5,
+				}}
+				className={style.judgesTopTen}
+			>
+				<h1>HackFest 2024 Top 10 Judges</h1>
+				<p>Let&#39;s meet our Official TOP 10 Judges</p>
+			</motion.div>
+			<motion.div 
+            initial={{ opacity: 0, y:150 }}
+            whileInView={{ opacity: 1, y:0 }}
+            transition={{ 
+                duration: 0.5
+            }}
+            className={style.mentorsTen}>
+            {toptens.map((ten, i) => (
+                <div className={style.mentorsColTen} key={i}>
+                    <div className='mentorsImg'>
+                        <img className='mentorsBorder' src="/libraries/images/svg/circleMentors.svg" alt="" />
+                        <img className='mentorsRealImg' src={ten.image} alt="" />
+                    </div>
+                    <h1>{ten.name}</h1>
+                    <p>{ten.role}</p>
                 </div>
             ))}
         </motion.div>
